37f894c14806c078eb85124b2f323dee81974344
MarianaCRuiz <mchierandaruiz0gmail.com>
2022-01-11 15:12:11 -0300

    removing some comments

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,12 +3,4 @@ class Report < ApplicationRecord
   enum file_condition: { failure: 10, success: 100 }
   validates :address, :report_type, :report_code, :file_condition, presence: true
   validates :report_code, uniqueness: true
-
-  # def self.generate_code
-  #   loop do
-  #     @code = SecureRandom.base58(8)
-  #     break unless Report.exists?(report_code: @code)
-  #   end
-  #   @code
-  # end
 end
